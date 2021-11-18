a0d2026d8d28acf9a3edc75498f66123cdf74f22
Peter DeWitt <peter.dewitt@nrel.gov>
2021-11-18 16:48:45 -0700

    add profile decoration to main

--- run.py
+++ run.py
@@ -15,6 +15,27 @@ import sys
 import warnings
 import numpy_financial as npf
 
+################################################################################
+# Define a decorator for profiling functions
+import cProfile, pstats, io
+
+def profile(fnc):
+    """A decorator that uses cProfile to profile a function"""
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream = s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+    return inner
+
+################################################################################
+
 
 class UsefulInputFiles(object):
     """Class of input files to be opened by this routine.
@@ -3840,6 +3861,7 @@ class Engine(object):
         return adjust_dict
 
 
+@profile
 def main(base_dir):
     """Import, finalize, and write out measure savings and financial metrics.
 
