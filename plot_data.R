7a409c1fe578cf77647e18395833eb77c60ef31e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-01-25 17:32:52 -0700

    outline data cleaning and formatting of markets and savings overall

--- plot_data.R
+++ plot_data.R
@@ -69,9 +69,62 @@ for(i in seq_along(ecm_prep)) {
   ecm_prep[[i]][["name"]] <- NULL
 }
 
+################################################################################
+###                           Part out ECM Results                           ###
+
+# There is (are) non ecm related results in the ecm_results object, move them to
+# a standalone object.
+non_ecm_results <- ecm_results[setdiff(names(ecm_results), names(ecm_prep))]
+ecm_results[setdiff(names(ecm_results), names(ecm_prep))] <- NULL
+stopifnot(setequal(names(ecm_results), names(ecm_prep)))
+
+filter_variables <-
+  lapply(ecm_results, getElement, "Filter Variables")
+markets_savings_overall <-
+  lapply(ecm_results, getElement, "Markets and Savings (Overall)")
+markets_savings_by_category <-
+  lapply(ecm_results, getElement, "Markets and Savings (By Category)")
+financial_metrics <-
+  lapply(ecm_results, getElement, "Financial Metrics")
+
+################################################################################
+###                         Clean "Filter Variables"                         ###
+
+str(filter_variables, max.level = 1)
+str(filter_variables[[1]], max.level = 1)
+
+################################################################################
+###                   Clean Markets and Savings (Overall)                    ###
+
+# append the ecm_results with the "All ECMs" results
+markets_savings_overall[["All ECMs"]] <-
+  agg_results[["All ECMs"]][["Markets and Savings (Overall)"]]
+
+# coerce the year/value data into a data.table, this will be in a "wide" format
+markets_savings_overall <-
+  lapply(markets_savings_overall, lapply, lapply, data.table::as.data.table)
+
+# pivot the data.tables to a "long format"
+markets_savings_overall <-
+  lapply(markets_savings_overall, lapply, lapply,
+         function(x) {
+           data.table::melt(x, measure.vars = names(x),
+                            variable.factor = FALSE, variable.name = "year")
+         })
+
+# bind all the results into one data.table, level by level
+markets_savings_overall <-
+  lapply(markets_savings_overall, lapply,
+         data.table::rbindlist, idcol = "variable")
+markets_savings_overall <-
+  lapply(markets_savings_overall,
+         data.table::rbindlist, idcol = "adoption_scenario")
+markets_savings_overall <-
+  data.table::rbindlist(markets_savings_overall, idcol = "ecm")
+
+################################################################################
 # save the object as a .rds object
 saveRDS(ecm_prep, file = "./results/plots/ecm_prep.rds")
-
 
 ################################################################################
 ###                               End of File                                ###
