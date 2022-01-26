3f1b238d68f2027be486207ba2e0b6d560f2ef63
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-01-26 15:10:37 -0700

    add script for generating financial metrics as a data.frame

---
+++ results/plots/plotting_scripts/financial_metrics.R
@@ -0,0 +1,34 @@
+################################################################################
+###                         Clean Finaancial Metrics                         ###
+
+# Import competed ECM energy, carbon, and cost data
+ecm_results <-
+  rjson::fromJSON(file = file.path('.', 'results','ecm_results.json'))
+
+# extract the financial_metrics
+financial_metrics <- lapply(ecm_results, getElement, "Financial Metrics")
+
+# transfrom from the json form to a data.table
+financial_metrics <-
+  lapply(financial_metrics, lapply, data.table::as.data.table)
+
+financial_metrics <-
+  lapply(financial_metrics,
+         data.table::rbindlist,
+         idcol = "variable")
+
+financial_metrics <-
+  data.table::rbindlist(financial_metrics, idcol = "ecm")
+
+financial_metrics <-
+  data.table::melt(financial_metrics,
+                   id.vars = c("ecm", "variable"),
+                   variable.factor = FALSE,
+                   variable.name = "year")
+
+saveRDS(financial_metrics, file = "./results/plots/financial_metrics.rds")
+
+################################################################################
+###                               End of File                                ###
+################################################################################
+
