70cfeeab773b185cd5269e9110186697b173f419
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-01-25 17:48:22 -0700

    outline data cleaning for markets and savings by category

--- plot_data.R
+++ plot_data.R
@@ -83,15 +83,15 @@ filter_variables <-
 markets_savings_overall <-
   lapply(ecm_results, getElement, "Markets and Savings (Overall)")
 markets_savings_by_category <-
-  lapply(ecm_results, getElement, "Markets and Savings (By Category)")
+  lapply(ecm_results, getElement, "Markets and Savings (by Category)")
 financial_metrics <-
   lapply(ecm_results, getElement, "Financial Metrics")
 
 ################################################################################
 ###                         Clean "Filter Variables"                         ###
 
-str(filter_variables, max.level = 1)
-str(filter_variables[[1]], max.level = 1)
+# str(filter_variables, max.level = 1)
+# str(filter_variables[[1]], max.level = 1)
 
 ################################################################################
 ###                   Clean Markets and Savings (Overall)                    ###
@@ -121,6 +121,49 @@ markets_savings_overall <-
          data.table::rbindlist, idcol = "adoption_scenario")
 markets_savings_overall <-
   data.table::rbindlist(markets_savings_overall, idcol = "ecm")
+
+################################################################################
+###                 Clean Markets and Savings (By Category)                  ###
+
+str(markets_savings_by_category, max.level = 1)
+str(markets_savings_by_category[[1]], max.level = 1)
+str(markets_savings_by_category[[1]][[1]], max.level = 1)
+str(markets_savings_by_category[[1]][[2]], max.level = 1)
+str(markets_savings_by_category[[1]][[2]][[1]][[1]][[1]][[1]], max.level = 1)
+
+
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         lapply, lapply, lapply, lapply, lapply,
+         data.table::as.data.table)
+
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         lapply, lapply, lapply, lapply, lapply,
+         function(x) {
+           data.table::melt(x,
+                            measure.vars = names(x),
+                            variable.factor = FALSE,
+                            variable.name = "year")
+         })
+
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         lapply, lapply, lapply, lapply, data.table::rbindlist, idcol = "end_use")
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         lapply, lapply, lapply, data.table::rbindlist, idcol = "building_class")
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         lapply, lapply, data.table::rbindlist, idcol = "region")
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         lapply, data.table::rbindlist, idcol = "variable")
+markets_savings_by_category <-
+  lapply(markets_savings_by_category,
+         data.table::rbindlist, idcol = "adoption_scenario")
+markets_savings_by_category <-
+  data.table::rbindlist(markets_savings_by_category, idcol = "ecm")
 
 ################################################################################
 # save the object as a .rds object
