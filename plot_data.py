9115f7c56e75eab3e6f67c0ef0ee8ef6399d42f8
Peter DeWitt <peter.dewitt@nrel.gov>
2022-02-04 15:16:31 -0700

    wip

--- plot_data.py
+++ plot_data.py
@@ -6,7 +6,9 @@
 #
 # Build the following data sets:
 #
-# 1. fm -- Financial Metrics
+# 1. fm  -- Financial Metrics
+# 2. cms -- Competed Market Savings
+# 3. ums -- Uncompeted Market Savings
 #
 ################################################################################
 
@@ -18,11 +20,15 @@ f = open("./results/ecm_results.json")
 ecm_results = json.load(f)
 f.close()
 
-# Get the keys for each level
-ecm_keys  = list(ecm_results)
-ecm_keys.remove('On-site Generation')
+f = open("./supporting_data/ecm_prep.json")
+ecm_prep = json.load(f)
+f.close()
 
-lvl2_keys = list(ecm_results[ecm_keys[0]].keys()) 
+# Get the keys for each level
+ecm_results_keys  = list(ecm_results)
+ecm_results_keys.remove('On-site Generation')
+
+lvl2_keys = list(ecm_results[ecm_results_keys[0]].keys())
 
 # There are several different possible trees from here
 # >>> lvl2_keys
@@ -34,21 +40,26 @@ lvl2_keys = list(ecm_results[ecm_keys[0]].keys())
 ###                            Financial Metrics                             ###
 
 # explore the data structure a bit
-fm_variable_keys = list(ecm_results[ecm_keys[0]]["Financial Metrics"].keys())
+fm_variable_keys = \
+        list(ecm_results[ecm_results_keys[0]]["Financial Metrics"].keys())
 fm_variable_keys
 
 # yearly data is at this following levels
 for k in fm_variable_keys:
-    list(ecm_results[ecm_keys[0]]["Financial Metrics"][k].keys())
+    list(ecm_results[ecm_results_keys[0]]["Financial Metrics"][k].keys())
 
 # create the fm DataFrame
-fm = pd.Dataframe() 
-for ecm in ecm_keys:
+ecm_count = 1
+fm = pd.DataFrame()
+for ecm in ecm_results_keys:
+    print("(" + str(ecm_count) + "/" + str(len(ecm_results_keys)) +\
+            ") Extracting Financial Metrics for: " + ecm)
+    ecm_count = ecm_count + 1
     for v in fm_variable_keys:
         x = pd.DataFrame.from_dict(
                 ecm_results[ecm]["Financial Metrics"][v]
                 , orient = 'index'
-                , columns = ["value"]
+                , columns = ['value']
                 )
         x['variable'] = v
         x['ecm'] = ecm
@@ -57,25 +68,108 @@ for ecm in ecm_keys:
 # reset the index and rename -- the index is the year.
 fm.reset_index(inplace = True)
 fm.rename(columns = {"index" : "year"}, inplace = True)
-fm
 
+print("Writing ./results/plots/financial_metrics.csv")
+fm.to_csv("./results/plots/financial_metrics.csv")
 
 ################################################################################
-###                            Financial Metrics                             ###
+##                         Competed Market Savings                          ###
+CMS = "Markets and Savings (by Category)"
 
+ecm_count = 1
 
-lvl3_keys = list(ecm_results[ecm_keys[0]][lvl2_keys[
+cms = pd.DataFrame()
+for ecm in ecm_results_keys:
+    print("(" + str(ecm_count) + "/" + str(len(ecm_results_keys)) +\
+            ") Extracting Competed Markets Savings for: " + ecm)
+    ecm_count = ecm_count + 1
+    df1 = pd.DataFrame()
+    for ap in list(ecm_results[ecm][CMS].keys()):
+        df2 = pd.DataFrame()
+        for v in list(ecm_results[ecm][CMS][ap].keys()):
+            df3 = pd.DataFrame()
+            for cz in list(ecm_results[ecm][CMS][ap][v].keys()):
+                df4 = pd.DataFrame()
+                for bg in list(ecm_results[ecm][CMS][ap][v][cz].keys()):
+                    df5 = pd.DataFrame()
+                    for eu in list(ecm_results[ecm][CMS][ap][v][cz][bg].keys()):
+                        x = pd.DataFrame.from_dict(
+                                ecm_results[ecm][CMS][ap][v][cz][bg][eu]
+                                , orient = 'index'
+                                , columns = ['value']
+                                )
+                        x['end_use'] = eu
+                        x['building_class'] = bg
+                        x['climate_zone'] = cz
+                        x['variable'] = v
+                        x['adoption_scenario'] = ap
+                        x['ecm'] = ecm
+                        df5 = pd.concat([df5, x])
+                    df4 = pd.concat([df4, df5])
+                df3 = pd.concat([df3, df4])
+            df2 = pd.concat([df2, df3])
+        df1 = pd.concat([df1, df2])
+    cms = pd.concat([cms, df1])
 
-ecm_results[ecms[0]]["Markets and Savings (by Category)"].keys()
-ecm_results[ecms[0]]["Markets and Savings (by Category)"]['Max adoption potential'].keys()
-ecm_results[ecms[0]]["Markets and Savings (by Category)"]['Max adoption potential']["Baseline Energy Use (MMBtu)"].keys()
-ecm_results[ecms[0]]["Markets and Savings (by Category)"]['Max adoption potential']["Baseline Energy Use (MMBtu)"]["AIA CZ1"].keys()
-ecm_results[ecms[0]]["Markets and Savings (by Category)"]['Max adoption potential']["Baseline Energy Use (MMBtu)"]["AIA CZ1"]["Commercial (New)"].keys()
-ecm_results[ecms[0]]["Markets and Savings (by Category)"]['Max adoption potential']["Baseline Energy Use (MMBtu)"]["AIA CZ1"]["Commercial (New)"]["Heating (Equip.)"].keys()
+# reset the index and rename -- the index is the year.
+cms.reset_index(inplace = True)
+cms.rename(columns = {"index" : "year"}, inplace = True)
 
-ecm_results[ecms[0]]["Markets and Savings (by Category)"]['Max adoption potential']["Baseline Energy Use (MMBtu)"]["AIA CZ1"]["Commercial (New)"]["Heating (Equip.)"]
+print("Writing ./results/plots/competed_market_savings.csv")
+cms.to_csv("./results/plots/competed_market_savings.csv")
 
+################################################################################
+###                        Uncompeted Market Savings                         ###
 
+ums = pd.DataFrame()
+for i in range(len(ecm_prep)):
+    ecm = ecm_prep[i]["name"]
+    print("(" + str(i + 1) + "/" + str(len(ecm_prep)) +\
+            ") Extracting Uncompeted Markets Savings for: " + ecm)
+    df1 = pd.DataFrame()
+    for ap in list(ecm_prep[i]["markets"].keys()):
+        df2 = pd.DataFrame()
+        for ecc in list(ecm_prep[i]["markets"][ap]["mseg_out_break"].keys()):
+            df3 = pd.DataFrame()
+            for rs in list(ecm_prep[i]["markets"][ap]["mseg_out_break"][ecc]\
+                    .keys()):
+                df4 = pd.DataFrame()
+                for cz in list(ecm_prep[i]["markets"][ap]["mseg_out_break"]\
+                        [ecc][rs].keys()):
+                    df5 = pd.DataFrame()
+                    for bc in list(ecm_prep[i]["markets"][ap]["mseg_out_break"]\
+                            [ecc][rs][cz].keys()):
+                        df6 = pd.DataFrame()
+                        for eu in list(ecm_prep[i]["markets"][ap]\
+                                ["mseg_out_break"][ecc][rs][cz][bc].keys()):
+                            d = ecm_prep[i]["markets"][ap]["mseg_out_break"]\
+                                    [ecc][rs][cz][bc][eu]
+                            if (len(d) > 0):
+                                x = pd.DataFrame.from_dict(
+                                        d
+                                        , orient = 'index'
+                                        , columns = ['value'])
+                                x['end_use'] = eu
+                                x['building_class'] = bc
+                                x['climate_zone'] = cz
+                                x['results_scenario'] = rs
+                                x['ecc'] = ecc
+                                x['adoption_scenario'] = ap
+                                x['ecm'] = ecm
+                                df6 = pd.concat([df6, x])
+                        df5 = pd.concat([df5, df6])
+                    df4 = pd.concat([df4, df5])
+                df3 = pd.concat([df3, df4])
+            df2 = pd.concat([df2, df3])
+        df1 = pd.concat([df1, df2])
+    ums = pd.concat([ums, df1])
+
+# reset the index and rename -- the index is the year.
+ums.reset_index(inplace = True)
+ums.rename(columns = {"index" : "year"}, inplace = True)
+
+print("Writing ./results/plots/uncompeted_market_savings.csv")
+ums.to_csv("./results/plots/uncompeted_market_savings.csv")
 
 ################################################################################
 ###                               End of File                                ###
