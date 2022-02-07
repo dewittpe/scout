21514d557eff029aad2c17cf0e98854c9ec97bcf
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-02-07 10:30:36 -0700

    add script for building the cost effective plots for avoided co2

---
+++ plot_cost_effective_avoided_co2.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import plotly.express as px
+
+fm = pd.read_parquet("./results/plots/financial_metrics.parquet")
+cms = pd.read_parquet("./results/plots/competed_market_savings.parquet")
+
+fm.sort_values(by = ['ecm', 'variable', 'year'], inplace = True)
+cms.sort_values(by = ['ecm', 'variable', 'year'], inplace = True)
+
+# copy the variable column to be used as a facetting value
+fm['facet_row'] = fm["variable"]
+
+fm.loc[fm["variable"] == "Cost of Conserved CO\u2082 ($/MTon CO\u2082 avoided)"
+        , "facet_row"] =\
+                "Cost of Conserved CO\u2082<br>($/MTon CO\u2082 avoided)"
+
+fm.loc[fm["variable"] == "Cost of Conserved Energy ($/MMBtu saved)"
+        , "facet_row"] =\
+                "Cost of Conserved Energy<br>($/MMBtu saved)"
+
+# subset to avoided CO2
+cms = cms[cms["variable"] == "Avoided CO\u2082 Emissions (MMTons)"]
+
+# aggregate by building type
+def unique_strings(l):
+    list_set = set(l)
+    ul = (list(list_set))
+    return '; '.join(ul)
+
+cms =\
+    cms.groupby(["adoption_scenario", "ecm", "variable", "year"])\
+            .agg({
+                "value": "sum",
+                "building_class" : unique_strings,
+                "region" : unique_strings,
+                "end_use" : unique_strings
+                  })
+
+cms.reset_index(inplace = True)
+
+cms = \
+    pd.pivot_table(cms,
+            values = "value",
+            index = ["adoption_scenario", "ecm", "building_class", "end_use",
+                "year"],
+            columns = ["variable"])
+
+cms.reset_index(inplace = True)
+
+plot_data = cms.merge(fm, how = "left", on = ["ecm", "year"])
+plot_data["year"] = plot_data["year"].astype(str).astype(int)
+
+
+def plot_year(y1 = 2022, y2 = 2050):
+    fig = px.scatter(
+            plot_data[(plot_data["year"] >= y1) & (plot_data["year"] <= y2)]
+            , x = "Avoided CO₂ Emissions (MMTons)"
+            , y = "value"
+            , symbol = "building_class"
+            , color = "end_use"
+            , facet_col = "year"
+            , facet_row = "facet_row"
+            , hover_data = {
+                "ecm": True,
+                "Avoided CO₂ Emissions (MMTons)": True,
+                "value": True,
+                "end_use": True,
+                "building_class": True
+                }
+            )
+    fig.update_yaxes(matches = None)
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    return(fig)
+
+y1 = 2022
+while (y1 <= max(plot_data["year"])):
+    plot_file = "./results/plots/cost_effective_avoided_co2_" + str(y1) +\
+            "_" + str(y1 + 3) + ".html"
+    print("Writing " + plot_file)
+    plot_year(y1 = y1, y2 = y1 + 3).write_html(plot_file)
+    y1 = y1 + 4
+
+
+
